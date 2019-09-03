8e8d1c359036a49dd5f542265cccf413063c0855
Rachel T <rachmaybe@gmail.com>
2019-09-03 03:02:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,21 @@
 class Song 
   attr_accessor :name, :artist, :genre
+  def name 
+    @name = name 
+  end 
+  def name
+    @name 
+  end 
+  def artist
+    @artist = artist 
+  end 
+  def artist
+    @artist
+  end 
+  def genre
+    @genre = genre 
+  end 
+  def genre
+    @genre 
+  end 
 end 
